1bb31d9151ad47940faed36db117651583756f7b
João Vitor <joao_vitorsgs@hotmail.com>
2026-03-03 22:34:19 -0300

    feat(system/app): add metodo 'runMigrations' no contrato ITenantRepository

--- src/system/application/repositories/tenant.repository.ts
+++ src/system/application/repositories/tenant.repository.ts
@@ -4,4 +4,5 @@ import { Tenant } from "@/system/domain/entities/tenant";
 
 export interface ITenantRepository extends ISetUnitOfWork {
     createTenant(tenant: Tenant): Promise<void>;
+    runMigrations(migrationsPath: string[]): Promise<void>;
 }
