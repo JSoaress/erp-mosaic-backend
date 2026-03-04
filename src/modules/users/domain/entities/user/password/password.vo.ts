65e6905be97a3759c0a9c06c0afa2cdd6606b3e4
João Vitor <joao_vitorsgs@hotmail.com>
2026-03-04 13:18:01 -0300

    feat(users/app): add value object Password

---
+++ src/modules/users/domain/entities/user/password/password.vo.ts
@@ -0,0 +1,32 @@
+import bcrypt from "bcrypt";
+import { Either, left, right } from "ts-arch-kit/dist/core/helpers";
+
+import { InvalidPasswordError } from "@/shared/errors";
+
+import { PasswordProps } from "./types";
+
+export class Password {
+    private constructor(private value: string) {}
+
+    static async create(plainPassword: string, props?: PasswordProps): Promise<Either<InvalidPasswordError, Password>> {
+        if (!plainPassword) return left(new InvalidPasswordError("Senha não fornecida."));
+        if (props?.policy) {
+            const isValidOrError = props.policy.validate(plainPassword);
+            if (isValidOrError.isLeft()) return left(isValidOrError.value);
+        }
+        const hashPassword = await bcrypt.hash(plainPassword, props?.hashRounds || 12);
+        return right(new Password(hashPassword));
+    }
+
+    static restore(password: string) {
+        return new Password(password);
+    }
+
+    async verify(plainPassword: string): Promise<boolean> {
+        return bcrypt.compare(plainPassword, this.value);
+    }
+
+    getValue() {
+        return this.value;
+    }
+}
