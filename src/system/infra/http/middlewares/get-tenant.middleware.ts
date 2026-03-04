8efd8857f609301250e0ee4fecb48f073755f6d3
João Vitor <joao_vitorsgs@hotmail.com>
2026-03-04 12:21:14 -0300

    feat(system/http): add middleware 'getTenant'

---
+++ src/system/infra/http/middlewares/get-tenant.middleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+
+import { TenantIdNotProvidedError } from "@/shared/errors";
+import { GetSubscriberUseCase } from "@/system/application/use-cases/subscriber/get-subscriber";
+
+export function getTenant(getSubscriberUseCase: GetSubscriberUseCase) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        if (req.originalUrl.startsWith("/api/system")) return next();
+        const tenantId = req.headers["x-tenant-id"] as string;
+        if (!tenantId) return next(new TenantIdNotProvidedError());
+        const response = await getSubscriberUseCase.execute({ queryOptions: { filter: { document: tenantId } } });
+        if (response.isLeft()) return next(response.value);
+        const subscriber = response.value;
+        req.subscriber = subscriber;
+        req.tenant = subscriber.getTenant();
+        return next();
+    };
+}
