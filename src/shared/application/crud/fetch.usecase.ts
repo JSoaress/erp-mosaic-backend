b534716927a6e4a47cd3b8c2f1eebe582527fad2
João Vitor <joao_vitorsgs@hotmail.com>
2026-03-09 22:25:05 -0300

    feat(shared): add classe FetchUseCase

---
+++ src/shared/application/crud/fetch.usecase.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Either, right } from "ts-arch-kit/dist/core/helpers";
+import { QueryOptions } from "ts-arch-kit/dist/database";
+
+import { MosaicError } from "@/shared/errors";
+import { Pagination } from "@/shared/helpers";
+import { IBaseRepositoryFactory, IRepository } from "@/shared/infra/database";
+import { Tenant } from "@/system/domain/entities/tenant";
+
+import { UseCase } from "../usecase";
+
+type FetchUseCaseGateway<Repo extends IBaseRepositoryFactory> = {
+    repositoryFactory: Repo;
+    repo: keyof Omit<Repo, "createUnitOfWork">;
+};
+
+export type FetchUseCaseInput = {
+    queryOptions?: QueryOptions;
+    tenant: Tenant;
+};
+
+export type FetchUseCaseOutput = Either<MosaicError, Pagination<any>>;
+
+export abstract class FetchUseCase<
+    TInput extends FetchUseCaseInput,
+    TOutput extends FetchUseCaseOutput,
+    TRepositoryFactory extends IBaseRepositoryFactory,
+> extends UseCase<TInput, TOutput> {
+    constructor(protected gateway: FetchUseCaseGateway<TRepositoryFactory>) {
+        super();
+    }
+
+    protected async impl({ queryOptions, tenant }: TInput): Promise<TOutput> {
+        const unitOfWork = this.gateway.repositoryFactory.createUnitOfWork(tenant);
+        const repository = (this.gateway.repositoryFactory[this.gateway.repo] as any)() as IRepository<any>;
+        unitOfWork.prepare(repository);
+        return unitOfWork.execute<TOutput>(async () => {
+            const count = await repository.count(queryOptions?.filter);
+            const results = await repository.find(queryOptions);
+            return right(new Pagination(count, results)) as TOutput;
+        });
+    }
+}
