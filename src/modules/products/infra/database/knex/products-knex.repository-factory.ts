06c3f87213ce4e93a21225d8a9d570e1a549cd0a
João Vitor <joao_vitorsgs@hotmail.com>
2026-03-10 19:25:19 -0300

    feat(products/db): impl metodo 'createModelRepository' na classe ProductsKnexRepositoryFactory

--- src/modules/products/infra/database/knex/products-knex.repository-factory.ts
+++ src/modules/products/infra/database/knex/products-knex.repository-factory.ts
@@ -1,7 +1,7 @@
 import type { Knex } from "knex";
 import { UnitOfWork } from "ts-arch-kit/dist/database";
 
-import { IBrandRepository, IRepositoryFactory } from "@/modules/products/application/repositories";
+import { IBrandRepository, IModelRepository, IRepositoryFactory } from "@/modules/products/application/repositories";
 import { getKnex } from "@/shared/infra/database/knex/knexconfig";
 import { DefaultKnexRepository, KnexUnitOfWork } from "@/shared/infra/database/knex/repositories";
 import { Tenant } from "@/system/domain/entities/tenant";
@@ -20,7 +20,7 @@ export class ProductsKnexRepositoryFactory implements IRepositoryFactory {
         return new DefaultKnexRepository("products_brands", new mappers.BrandKnexMapper());
     }
 
-    // createModelRepository(): IModelRepository {
-    //     return new DefaultKnexRepository("products_models", new mappers.ModelKnexMapper());
-    // }
+    createModelRepository(): IModelRepository {
+        return new DefaultKnexRepository("products_models", new mappers.ModelKnexMapper());
+    }
 }
