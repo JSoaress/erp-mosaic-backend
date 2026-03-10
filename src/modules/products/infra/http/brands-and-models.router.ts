9a47fde734ab275a9f41a67b4c4dba3d43fa68b1
João Vitor <joao_vitorsgs@hotmail.com>
2026-03-10 19:26:14 -0300

    feat(products/http): add rotas ref ao CRUD da entity Model

--- src/modules/products/infra/http/brands-and-models.router.ts
+++ src/modules/products/infra/http/brands-and-models.router.ts
@@ -4,14 +4,14 @@ import { ProductsUseCaseFactory } from "../../application/factories";
 import { EntityJsonPresenter } from "./presenters";
 
 export function brandsAndModelsRouter(router: Router, useCaseFactory: ProductsUseCaseFactory) {
-    const brandJsonPresenter = new EntityJsonPresenter();
+    const defaultJsonPresenter = new EntityJsonPresenter();
 
     router.get("/brands", async (req, res, next) => {
         const { tenant } = req;
         const useCase = useCaseFactory.fetchBrandsUseCase();
         const response = await useCase.execute({ tenant });
         if (response.isLeft()) return next(response.value);
-        return res.status(200).json(response.value.getData(brandJsonPresenter));
+        return res.status(200).json(response.value.getData(defaultJsonPresenter));
     });
 
     router.post("/brands", async (req, res, next) => {
@@ -19,7 +19,7 @@ export function brandsAndModelsRouter(router: Router, useCaseFactory: ProductsUs
         const useCase = useCaseFactory.createBrandUseCase();
         const response = await useCase.execute({ ...req.body, tenant });
         if (response.isLeft()) return next(response.value);
-        return res.status(201).json(brandJsonPresenter.present(response.value));
+        return res.status(201).json(defaultJsonPresenter.present(response.value));
     });
 
     router.patch("/brands/:brand", async (req, res, next) => {
@@ -28,7 +28,7 @@ export function brandsAndModelsRouter(router: Router, useCaseFactory: ProductsUs
         const useCase = useCaseFactory.updateBrandUseCase();
         const response = await useCase.execute({ ...req.body, id: brand, tenant });
         if (response.isLeft()) return next(response.value);
-        return res.status(200).json(brandJsonPresenter.present(response.value));
+        return res.status(200).json(defaultJsonPresenter.present(response.value));
     });
 
     router.delete("/brands/:brand", async (req, res, next) => {
@@ -40,12 +40,39 @@ export function brandsAndModelsRouter(router: Router, useCaseFactory: ProductsUs
         return res.status(204).send();
     });
 
-    // router.post("/brands/:brand/models", async (req, res, next) => {
-    //     const { tenant } = req;
-    //     const { brand } = req.params;
-    //     const useCase = useCaseFactory.createModelUseCase();
-    //     const response = await useCase.execute({ ...req.body, brandId: brand, tenant });
-    //     if (response.isLeft()) return next(response.value);
-    //     return res.status(201).json(response.value);
-    // });
+    router.get("/brands/:brand/models", async (req, res, next) => {
+        const { tenant } = req;
+        const { brand } = req.params;
+        const useCase = useCaseFactory.fetchModelsUseCase();
+        const response = await useCase.execute({ queryOptions: { filter: { brandId: parseInt(brand, 10) } }, tenant });
+        if (response.isLeft()) return next(response.value);
+        return res.status(200).json(response.value.getData(defaultJsonPresenter));
+    });
+
+    router.post("/brands/:brand/models", async (req, res, next) => {
+        const { tenant } = req;
+        const { brand: brandId } = req.params;
+        const useCase = useCaseFactory.createModelUseCase();
+        const response = await useCase.execute({ ...req.body, brandId, tenant });
+        if (response.isLeft()) return next(response.value);
+        return res.status(201).json(defaultJsonPresenter.present(response.value));
+    });
+
+    router.patch("/brands/:brand/models/:model", async (req, res, next) => {
+        const { tenant } = req;
+        const { brand: brandId, model } = req.params;
+        const useCase = useCaseFactory.updateModelUseCase();
+        const response = await useCase.execute({ ...req.body, id: model, brandId, tenant });
+        if (response.isLeft()) return next(response.value);
+        return res.status(200).json(defaultJsonPresenter.present(response.value));
+    });
+
+    router.delete("/brands/:brand/models/:model", async (req, res, next) => {
+        const { tenant } = req;
+        const { brand: brandId, model } = req.params;
+        const useCase = useCaseFactory.deleteModelUseCase();
+        const response = await useCase.execute({ id: model, brandId, tenant });
+        if (response.isLeft()) return next(response.value);
+        return res.status(204).send();
+    });
 }
