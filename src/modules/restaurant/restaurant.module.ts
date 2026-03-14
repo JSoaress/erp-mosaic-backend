776b1798e5d9e388fb354ec54e61b424398a3c42
João Vitor <joao_vitorsgs@hotmail.com>
2026-03-14 14:09:13 -0300

    feat(restaurant): injeta dependencia 'contractRegistry' no entry point do modulo

--- src/modules/restaurant/restaurant.module.ts
+++ src/modules/restaurant/restaurant.module.ts
@@ -1,5 +1,6 @@
 import path from "node:path";
 
+import { ContractsRegistry, IProductsContract } from "@/core/contracts";
 import { ERPModule } from "@/core/module/erp-module.interface";
 import { ForeignKeyValidationService } from "@/shared/application/services";
 import knexConfig from "@/shared/infra/database/knex/knexfile";
@@ -8,10 +9,11 @@ import { RestaurantUseCaseFactory } from "./application/factories";
 import { RestaurantKnexRepositoryFactory } from "./infra/database/knex";
 import { createRouter } from "./infra/http";
 
-export function buildRestaurantModule() {
+export function buildRestaurantModule(contractRegistry: ContractsRegistry) {
     const repositoryFactory = new RestaurantKnexRepositoryFactory(knexConfig.development);
     const fkValidationService = new ForeignKeyValidationService();
-    const useCaseFactory = new RestaurantUseCaseFactory(repositoryFactory, fkValidationService);
+    const productsContract = contractRegistry.resolve<IProductsContract>("products");
+    const useCaseFactory = new RestaurantUseCaseFactory(repositoryFactory, fkValidationService, productsContract);
     const router = createRouter(useCaseFactory);
     const module = new ERPModule(
         { name: "restaurant" },
